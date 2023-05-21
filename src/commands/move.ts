bb0f5632d11f95d2e478f4a803f07e9d2a37a0d6
Kilomaster3 <Aliaksei.Hrom-ext@fidor.com>
2023-05-21 17:05:10 +0300

    Add navigation part

---
+++ src/commands/move.ts
@@ -0,0 +1,21 @@
+import robot from 'robotjs';
+
+export const moveMouse = (action, x, y, value) => {
+  switch (action) {
+    case 'mouse_up':
+      robot.moveMouse(x, y - Number(value[0]));
+      break;
+    case 'mouse_down':
+      robot.moveMouse(x, y + Number(value[0]));
+      break;
+    case 'mouse_left':
+      robot.moveMouse(x - Number(value[0]), y);
+      break;
+    case 'mouse_right':
+      robot.moveMouse(x + Number(value[0]), y);
+      break;
+    case 'mouse_position':
+      robot.moveMouse(x, y);
+      break;
+  }
+};
